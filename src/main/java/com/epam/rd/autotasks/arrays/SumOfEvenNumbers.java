8593043af4415d7b93f3ba08bd510be2fcd1de78
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-25 11:53:31 +0200

    done

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -1,17 +1,19 @@
 package com.epam.rd.autotasks.arrays;
 
 public class SumOfEvenNumbers {
+        public static void main(String[] args) {
+            int[]vals = new int[]{-2,10,0,5};
+            int result = SumOfEvenNumbers.sum(vals);
+            System.out.println(result == 8); // true
+        }
 
-    public static void main(String[] args) {
-        int[] array = new int[]{1, 3, 2, 8, 15, 199};
-
-        System.out.println(sum(array));
+        public static int sum(int[] array) {
+            int evenSum = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] % 2 == 0) {
+                    evenSum += array[i];
+                }
+            }
+            return evenSum;
+        }
     }
-
-    public static int sum(int[] array){
-
-        //put your code here
-
-        throw new UnsupportedOperationException();
-    }
-}
