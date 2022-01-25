d37c85b8ca403a3ec0fb782d83814b0b66470fc3
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-25 13:23:57 +0200

    amended

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -2,20 +2,16 @@ package com.epam.rd.autotasks.arrays;
 
 public class SumOfEvenNumbers {
     public static void main(String[] args) {
-        int[] vals = new int[]{-2, 10, 0, 5};
-        int result = SumOfEvenNumbers.sum(vals);
+
+        int[] arrayOfNumbers = new int[]{-2, 10, 0, 5};
+        int result = SumOfEvenNumbers.sum(arrayOfNumbers);
         System.out.println(result == 8); // true
     }
 
     public static int sum(int[] array) {
-        int evenSum = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (i % 2 == 0) {
-                evenSum += i;
-            } else if (array == null || array.length == 0) {
-                evenSum = 0;
-            }
-        }
-        return evenSum;
+        if (Arrays.stream(array).allMatch(v -> Objects.isNull(v))) {
+            int res = 0;
+            return res;
+        } else return Arrays.stream(array).filter(f -> f % 2 == 0).sum();
     }
 }
